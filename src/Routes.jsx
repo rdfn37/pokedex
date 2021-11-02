f23c8413c1606478b367f9c912d6aa404d3d4e14
rdfn37 <rdfn37@gmail.com>
2021-11-02 18:16:55 -0300

    Implementado página de informação dos pokemon

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,12 @@
 import { Switch, Route, Redirect } from "react-router";
 import Card from "./components/Card";
 import IndexPage from "./components/IndexPage";
-import CardPage from "./components/CardPage";
+import PokemonPage from "./components/PokemonPage"
 import React, { useEffect, useState } from "react";
 import { pokemon } from "./App";
 
 export default props =>
     <Switch>
         <Route exact path='/' component={IndexPage} />
-        <Route path='' component={CardPage} />
+        <Route path='' component={PokemonPage} />
     </Switch>
---
+++ src/components/PokemonPage.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/PokemonPage.css'
+
+const CardPage = () => {
+    const [pokemon, setPokemon] = useState({})
+
+    const [pokemonName, setPokemonName] = useState(window.location.pathname.substring(1))
+
+
+    const getPokemon = () => {
+        if (pokemonName) {
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+                .then(e => {
+                    setPokemon(e.data)
+                })
+        }
+    }
+
+
+    useEffect(() => {
+        getPokemon()
+    }, [])
+
+    //
+    const pokemonAbilities = pokemon.abilities
+    pokemonAbilities ? console.log(pokemonAbilities) : console.log('vazio')
+    //
+
+
+    return (
+        <React.Fragment>
+            <header className='pokemonPageHeader text-center'>
+                <h1>{pokemonName[0].toUpperCase() + pokemonName.substring(1)}</h1>
+            </header>
+
+            <div className='pokemonPageMain my-5'>
+
+                <div className={`pokemonPageImg ${pokemonName}-img mx-5`}>
+                    <img src={pokemon.sprites?.front_default} alt={pokemonName} />
+                </div>
+
+                <div className={`pokemonPageInfo ${pokemonName}-info text-center`}>
+
+                    <div className={`pokemonPageId ${pokemonName}-id`}>
+                        <h6>ID: {pokemon.id}</h6>
+                    </div>
+                    <hr />
+
+                    <div className={`pokemonPageType ${pokemonName}-type`}>
+                        <div className=''>
+                            {pokemon.types?.[0]?.type?.name} {' / ', pokemon.types?.[1]?.type?.name}
+                        </div>
+                    </div>
+                    <hr />
+
+                    <div className={`pokemonPageAbilities ${pokemonName}-abilities `}>
+                        <h6 className='mx-2 align-self-center'>Abilities</h6>
+                        <ul className='list-group'>
+                            <li className='list-group-item'>1 - {pokemon.abilities?.[0]?.ability?.name}</li>
+                            <li className='list-group-item'>2 - {pokemon.abilities?.[1]?.ability?.name}</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+
+                <div className={`pokemonPageStats ${pokemonName}-stats d-block`}>
+                    <table className='bg-dark rounded table table-danger'>
+                        <tr className="table table-danger col">
+                            <td className="text-light table table-danger">
+                                HP - {pokemon.stats?.[0]?.base_stat}
+                            </td>
+                        </tr>
+
+                        <tr className="table table-danger">
+                            <td className="text-light table table-danger">
+                                Attack - {pokemon.stats?.[1]?.base_stat}
+                            </td>
+                        </tr>
+
+                        <tr className="table table-danger">
+                            <td className="text-light table table-danger">
+                                Defense - {pokemon.stats?.[2]?.base_stat}
+                            </td>
+                        </tr>
+
+                        <tr className="table table-danger">
+                            <td className="text-light table table-danger">
+                                Sp. Attack - {pokemon.stats?.[3]?.base_stat}
+                            </td>
+                        </tr>
+
+                        <tr className="table table-danger">
+                            <td className="text-light table table-danger">
+                                Sp. Defense - {pokemon.stats?.[4]?.base_stat}
+                            </td>
+                        </tr>
+
+                        <tr className="table table-danger">
+                            <td className="text-light table table-danger">
+                                Speed - {pokemon.stats?.[5]?.base_stat}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <hr />
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default CardPage
